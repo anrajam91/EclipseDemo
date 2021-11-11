01ccd100986841173f27172147cda224a207cad7
anrajam91 <91654685+anrajam91@users.noreply.github.com>
2021-11-11 09:37:00 +0530

    Uploaded Week5 Day2 Assignments

---
+++ week5/day2/CreateLead.java
@@ -0,0 +1,33 @@
+package week5.day1;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CreateLead extends BaseClass {
+	@BeforeTest
+	public void setValue() {
+		// TODO Auto-generated method stub
+		excelFileName = "LeadTestData";
+		sheetName = "Sheet1";
+	}
+
+	@Test(dataProvider = "getData")
+	public void runCreateLead(String comp, String name, String lastname, String num) {
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys(comp);
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys(name);
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys(lastname);
+		driver.findElement(By.id("createLeadForm_primaryPhoneNumber")).sendKeys(num);
+		driver.findElement(By.name("submitButton")).click();
+	}
+
+}
---
+++ week5/day2/EditLead.java
@@ -0,0 +1,41 @@
+package week5.day1;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class EditLead  extends BaseClass {
+	@BeforeClass
+	public void setValue() {
+		// TODO Auto-generated method stub
+		excelFileName = "LeadTestData";
+		sheetName = "Sheet2";
+	}
+
+	 
+@Test(dataProvider="getData",dependsOnMethods="week5.day1.CreateLead.runCreateLead")
+
+	public void runEditLead(String num, String updName) throws InterruptedException {
+		driver.findElement(By.linkText("Find Leads")).click();
+		driver.findElement(By.xpath("//span[text()='Phone']")).click();
+		driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys(num);
+		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a")).click();
+		driver.findElement(By.linkText("Edit")).click();
+		driver.findElement(By.id("updateLeadForm_companyName")).sendKeys(updName);
+		driver.findElement(By.name("submitButton")).click();
+}
+}
+
+
+
+
+
+
